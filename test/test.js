268f00062a71142de0f50a803c6d543cefdd72c4
daiiz <da.iz.mg@gmail.com>
2017-06-04 13:12:46 +0900

    [[ ]] に成功した気がする

--- test/test.js
+++ test/test.js
@@ -8,14 +8,23 @@ console.info('test!');
 
 
 var BRACKET_OPEN = '[';
+var DOUBLE_BRACKET_OPEN = '[[';
 var BRACKET_CLOSE = ']';
+var DOUBLE_BRACKET_CLOSE = ']]';
 
 var tokens = [];
 var parse = function (nowstr, substr, seekCtrlStr) {
   console.info('INA>', nowstr);
   for (var j = 0; j < substr.length; j++) {
     var str = substr.substring(j, substr.length);
-    if (str.startsWith(BRACKET_OPEN)) {
+
+    if (str.startsWith(DOUBLE_BRACKET_OPEN)) {
+      var nextStr = str.substring(DOUBLE_BRACKET_OPEN.length, str.length);
+      var closedIdx = parse(str, nextStr, DOUBLE_BRACKET_CLOSE);
+      j += closedIdx;
+    }
+
+    else if (str.startsWith(BRACKET_OPEN)) {
       var nextStr = str.substring(BRACKET_OPEN.length, str.length);
       var closedIdx = parse(str, nextStr, BRACKET_CLOSE);
       j += closedIdx;
@@ -24,7 +33,9 @@ var parse = function (nowstr, substr, seekCtrlStr) {
     // 探していた閉じ記号が見つかった
     if (str.startsWith(seekCtrlStr)) {
       var closedIdx = j + seekCtrlStr.length;
-      console.info('GOT>', substr.substring(0, j));
+      var token = substr.substring(0, j);
+      console.info('GOT>', token);
+      tokens.push(decorate(token));
       return closedIdx;
     }
   }
@@ -32,16 +43,21 @@ var parse = function (nowstr, substr, seekCtrlStr) {
 
 // HTML文字列を返す
 var decorate = function (token) {
-
+  return token;
 }
 
 var parseRow = function (row) {
+  tokens = [];
   row = row.trim();
   for (var i = 0; i < row.length; i++) {
     var substr = row.substring(i, row.length);
-    if (substr.startsWith(BRACKET_OPEN)) {
+    if (substr.startsWith(DOUBLE_BRACKET_OPEN)) {
+      var closedIdx = parse(substr, substr, DOUBLE_BRACKET_CLOSE);
+      i += closedIdx;
+    }
+
+    else if (substr.startsWith(BRACKET_OPEN)) {
       var closedIdx = parse(substr, substr, BRACKET_CLOSE);
-      //console.info('GOT>', substr.substring(i, closedIdx));
       i += closedIdx;
     }
   }
