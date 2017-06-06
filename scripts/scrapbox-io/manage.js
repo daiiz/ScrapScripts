19e200da0a875ba03530ccf44a487b8f98f31c74
daiiz <da.iz.mg@gmail.com>
2017-06-06 17:26:54 +0900

    共通する関数を切り出した

--- scripts/scrapbox-io/manage.js
+++ scripts/scrapbox-io/manage.js
@@ -1,1 +1,12 @@
 // shared
+var installed = function (functionName) {
+  var d = `data-${functionName}`;
+  if ($('body').attr(d) && $('body').attr(d) === 'on') return true;
+  return false;
+};
+
+var detectProject = function () {
+  var r = window.location.href.match(/scrapbox.io\/([^\/.]*)/);
+  if (r && r.length >= 2) return window.encodeURIComponent(r[1]);
+  return 'daiiz';
+};
