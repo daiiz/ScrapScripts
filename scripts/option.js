c5e1ffeb779d9fc5355029fced9e41208a734032
daiiz <da.iz.mg@gmail.com>
2017-08-07 22:10:44 +0900

    一旦delete

--- scripts/option.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('#btn-main-project').addEventListener('click', function () {
-  var p = document.querySelector('#main-project').value;
-  if (p && p.length > 0) localStorage['main-project'] = p;
-}, false);
