09a4fa8a64fa1c5a6fb2e6e18b1d1f69f1f24c38
daiiz <da.iz.mg@gmail.com>
2018-03-08 00:34:45 +0900

    調整

--- src/scrapbox-io/stream-pane.js
+++ src/scrapbox-io/stream-pane.js
@@ -1,4 +1,6 @@
 const $ = require('jquery')
+window.jQuery = $
+require('../lib/split-pane.js')
 
 const matchScrapScript = url => {
   return url.match(/scrapbox\.io\/daiizscript\/([^\/\.]+)\.([^\/\.]+)$/)
@@ -18,6 +20,46 @@ const style = styles => {
   return base
 }
 
+const appendStage = () => {
+  const $html = $('html')
+  const $body = $('body')
+  const $stage = $(`
+  <div class="pretty-split-pane-frame">
+    <div class="split-pane fixed-left" style="min-width: 200px;">
+      <div class="split-pane-component" id="left-component" style="width: 35%;">
+      </div>
+      <div class="split-pane-divider" id="my-divider" style="left: 35%;">
+        <div class="split-pane-divider-inner"></div>
+      </div>
+      <div class="split-pane-component" id="right-component" style="left: 35%;">
+      </div>
+      <div class="split-pane-resize-shim" style="display: none;"></div></div>
+  </div>`)
+  $html.css({
+    height: '100%',
+    overflow: 'hidden'
+  })
+  $body.css({
+    height: '100%',
+    'margin-bottom': 0
+  })
+
+  // $('body').append('<div id="pane-dummy"></div>')
+  $('body').append($stage)
+
+  $('#my-divider').css({
+    'background-color': '#999'
+  })
+
+  $('#my-divider').on('mousedown', e => {
+    $('iframe').css('display', 'none')
+  })
+
+  $('body').on('mouseup', e => {
+    $('iframe').css('display', 'block')
+  })
+}
+
 exports.enable = () => {
   const $body = $('body')
   const href = window.location.href
@@ -26,23 +68,47 @@ exports.enable = () => {
   if (!matchScrapScript(href)) return
   const [_, featureName, projectName] = matchScrapScript(href)
   if (!featureName === 'stream') return
-  $body.html('')
+  $('#app-container').css({ display: 'none' })
+
+  appendStage()
 
   // main editor
   const $editor = $(document.createElement('iframe'))
   $editor.attr('src', `https://scrapbox.io/${projectName}`)
-  $editor.css(style({
-    width: 'calc(100% - 330px)',
-    left: 0
-  }))
-  $body.append($editor)
+  $editor.attr('id', 'm')
+  $editor.attr('tabindex', -1)
+  $editor.attr('class', 'pretty-split-pane-component-inner wm')
+  $('#left-component').append($editor)
+
+  const onClick = e => {
+    e.preventDefault()
+    e.stopPropagation()
+    const a = e.target.closest('a')
+    const url = a.dataset.href
+    $editor.attr('src', url)
+    return false
+  }
+
+  const onMouseenter = e => {
+    const a = e.target.closest('a')
+    if (a.dataset.href) return
+    console.log(a.href)
+    a.dataset.href = a.href
+    a.href = ''
+  }
 
   // side stream
   const $stream = $(document.createElement('iframe'))
   $stream.attr('src', `https://scrapbox.io/stream/${projectName}`)
-  $stream.css(style({
-    width: '330px',
-    right: 0
-  }))
-  $body.append($stream)
+  $stream.attr('id', 'mm')
+  $stream.attr('tabindex', -1)
+  $stream.attr('class', 'pretty-split-pane-component-inner em')
+  $('#right-component').append($stream)
+  $stream.on('load', function () {
+    $(this).contents().on('mouseenter', '.page a', onMouseenter)
+    $(this).contents().on('click', '.page a', onClick)
+  })
+
+  $('#app-container').html('')
+  $('div.split-pane').splitPane()
 }
