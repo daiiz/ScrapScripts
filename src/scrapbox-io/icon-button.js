a810e114e06ddd55f070bb074581b2fc373069b8
daiiz <da.iz.mg@gmail.com>
2018-07-13 10:33:31 +0900

    fix icon-button

--- src/scrapbox-io/icon-button.js
+++ src/scrapbox-io/icon-button.js
@@ -3,12 +3,19 @@ const daiizScrapboxManage = require('./manage')
 const installed = daiizScrapboxManage.installed
 const detectProject = daiizScrapboxManage.detectProject
 
+const isChrome = () => {
+  return /Chrome/.test(navigator.userAgent)
+}
+
+const isFirefox = () => {
+  return /Firefox/.test(navigator.userAgent)
+}
+
 exports.enable = () => {
   var $appRoot = $('#app-container')
 
-  $appRoot.on('click', 'img.icon', async function (e) {
+  $appRoot.on('click', 'img.icon', function (e) {
     if (!installed('daiiz-icon-button')) return
-    e.preventDefault()
     var projectName = detectProject()
 
     var $t = $(e.target).closest('img.icon')
@@ -22,9 +29,36 @@ exports.enable = () => {
       var buttonName = iconName[0]
       var scriptFilePath = `https://scrapbox.io/api/code/${projectName}/${buttonName}/button.js`
 
-      const res = await fetch(scriptFilePath, {credentials: 'include'})
-      const body = await res.text()
-      eval(body)
+      // const res = await fetch(scriptFilePath, {credentials: 'include'})
+      // const body = await res.text()
+      // eval(body)
+      // location.href = `https://scrapbox.io/${projectName}/${buttonName}`
+
+      // $.ajax({
+      //   type: 'GET',
+      //   url: scriptFilePath,
+      //   contentType: 'application/javascript; charset=UTF-8'
+      // }).done(body => {
+      //   const lines = body.split('\n')
+      //   const commands = lines[0]
+      //   if (isChrome() && commands.includes('--eval')) {
+      //     console.warn('chrome', commands)
+      //     eval(body)
+      //   } else if (isFirefox()) {
+      //     eval(body)
+      //   } else {
+      //     // 自動実行される
+      //   }
+      // })
+
+      const xhr = new XMLHttpRequest()
+      xhr.open('GET', scriptFilePath)
+      xhr.onload = function () {
+        const {response} = this
+        eval(response)
+      }
+      xhr.send()
+
       return false
     }
   })
